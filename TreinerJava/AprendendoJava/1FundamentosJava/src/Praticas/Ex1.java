0aa8e9c85bc7917cca69a97506b3f41e66f086e6
FelipeMovio <felipelsmovio@gmail.com>
2026-02-01 15:24:35 -0300

    ...

--- TreinerJava/AprendendoJava/1FundamentosJava/src/Praticas/Ex1.java
+++ TreinerJava/AprendendoJava/1FundamentosJava/src/Praticas/Ex1.java
@@ -1,5 +1,7 @@
 package Praticas;
 
+import java.util.Scanner;
+
 public class Ex1 {
     public static void main(String[] args) {
 
@@ -11,5 +13,26 @@ public class Ex1 {
 
         System.out.println(media);
 
+    }
+}
+
+class Ex1ComScaner{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" Digite nota 1 ");
+        Double nota1 = sc.nextDouble();
+
+        System.out.println(" Digite nota 2 ");
+        Double nota2 = sc.nextDouble();
+
+        System.out.println(" Digite nota 3 ");
+        Double nota3 = sc.nextDouble();
+
+        Double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println(" a media das notas sao : " + media);
+
+        sc.close();
     }
 }
