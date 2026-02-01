fc704d4d83203809a7034a6dcd24b7c6109ea3b6
FelipeMovio <felipelsmovio@gmail.com>
2026-02-01 15:05:08 -0300

    ..

--- TreinerJava/AprendendoJava/1FundamentosJava/src/Praticas/Ex1.java
+++ TreinerJava/AprendendoJava/1FundamentosJava/src/Praticas/Ex1.java
@@ -3,5 +3,8 @@ package Praticas;
 public class Ex1 {
     public static void main(String[] args) {
 
+        Number idade = 22;
+        Double Altura = 1.98;
+        String nome = "MArcquinho ";
     }
 }
