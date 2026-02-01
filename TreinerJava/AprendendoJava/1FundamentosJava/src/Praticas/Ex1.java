db156705057216e4d62deab55f17350c23442dcb
FelipeMovio <felipelsmovio@gmail.com>
2026-02-01 15:17:20 -0300

    treinando relembrando

--- TreinerJava/AprendendoJava/1FundamentosJava/src/Praticas/Ex1.java
+++ TreinerJava/AprendendoJava/1FundamentosJava/src/Praticas/Ex1.java
@@ -3,5 +3,13 @@ package Praticas;
 public class Ex1 {
     public static void main(String[] args) {
 
+        Double nota1 = 5.;
+        Double nota2 = 9.7;
+        Double nota3 = 8.2;
+
+        Double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println(media);
+
     }
 }
