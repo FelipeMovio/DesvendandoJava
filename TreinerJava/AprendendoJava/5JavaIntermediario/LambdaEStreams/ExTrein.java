4bb0f0e051684189ca758f0502f8d9f1dc620984
FelipeMovio <felipelsmovio@gmail.com>
2026-04-17 11:45:03 -0300

    ...

--- TreinerJava/AprendendoJava/5JavaIntermediario/LambdaEStreams/ExTrein.java
+++ TreinerJava/AprendendoJava/5JavaIntermediario/LambdaEStreams/ExTrein.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 public class ExTrein {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(10, 20, 30, 40, 50);
+        // código do agrupamento
 
         Integer maior = numeros.stream()
                 .max(Integer ::compareTo)
@@ -19,12 +20,14 @@ public class ExTrein {
 
         List<String> palavras = Arrays.asList("java", "stream", "lambda", "code");
 
+        // código do agrupamento
         Map<Integer, List<String>> agrupadoPorTamanho = palavras.stream()
                 .collect(Collectors.groupingBy(String::length));
 
         System.out.println(agrupadoPorTamanho);
 
         List<Integer> numeros2 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        // código da filtragem e agrupamento dos dados
 
         Integer paresX = numeros2.stream()
                 .filter(n -> n % 2 == 0)
@@ -32,6 +35,18 @@ public class ExTrein {
                 .sum();
         System.out.println(paresX);
 
+        List<Integer> numeros3 = Arrays.asList(1, 2, 3, 4, 5, 6);
+        // código do particionamento da lista
+
+        Map<Boolean, List<Integer>> particao = numeros3.stream()
+                .collect(Collectors.partitioningBy(n -> n % 2 == 0));
+
+        List<Integer> pares = particao.get(true);
+
+        List<Integer> impares = particao.get(false);
+
+        System.out.println("Pares: " + pares);
+        System.out.println("Ímpares: " + impares);
 
     }
 
